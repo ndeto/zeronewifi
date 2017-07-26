c95fe01b85e3a95b4cb3c6ba162d0da227d8fab7
Martin Ndeto <martindeto@gmail.com>
2017-07-26 09:59:04 +0300

    rake errors

--- db/migrate/20170721070703_change_data_type_for_splashimage_o_pacity.rb
+++ db/migrate/20170721070703_change_data_type_for_splashimage_o_pacity.rb
@@ -1,5 +1,5 @@
 class ChangeDataTypeForSplashimageOPacity < ActiveRecord::Migration[5.0]
   def change
-    change_column(:store_campaigns, :splashimage_opacity, :decimal(10,1))
+    change_column(:store_campaigns, :splashimage_opacity, :decimal, :precision => 10, :scale => 1)
   end
 end
