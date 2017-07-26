ae36a0094d51d459eaa46ae272a393e997459972
Martin Ndeto <martindeto@gmail.com>
2017-07-26 09:06:26 +0300

    edit campaign view

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,16 +1,37 @@
-jQuery(document).ready(function($) { 
-	
-	$('#op').change(function(e){
-		
-		
-		$('#splash').css("opacity",$('#op').val());
+jQuery(document).ready(function($) {
+
+	$('#op').change(function(e) {
+
+		$('#splash').css("opacity", $('#op').val());
 	});
-	
+
 	$('.colorpicker').colorpicker();
-	
-	$('.colorpicker').on('changeColor', function(ev){
-		
-  $('#clrbx').css("background-color",ev.color.toHex());
-});
-	
+
+	$('.colorpicker').on('changeColor', function(ev) {
+
+		$('#clrbx').css("background-color", ev.color.toHex());
 	});
+
+	$("#color").click(function() {
+		$(".back").css('display', 'none');
+		$(".backcolor").css('display', 'block');
+	});
+
+	$("#back").click(function() {
+		$(".back").css('display', 'block');
+		$(".backcolor").css('display', 'none');
+	});
+
+	if ($('#color').is(':checked')) {
+		
+		$(".back").css('display', 'none');
+		$(".backcolor").css('display', 'block');
+		
+	} else {
+		
+		$(".back").css('display', 'block');
+		$(".backcolor").css('display', 'none');
+		
+	}
+
+});
