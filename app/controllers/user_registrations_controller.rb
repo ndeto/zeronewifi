2c025e28d540cb1cfdcff1bb95e9c3170452945a
Martin Ndeto <martindeto@gmail.com>
2017-07-28 16:41:42 +0300

    confirmable

--- app/controllers/user_registrations_controller.rb
+++ app/controllers/user_registrations_controller.rb
@@ -8,4 +8,23 @@ class UserRegistrationsController < Devise::RegistrationsController
     end
 
   end
+  
+   # POST /resource
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:notice] = "User Created Successfully"
+      redirect_to(users_stores_path)
+    else
+      flash[:notice] = "Something Went wrong, Try again"
+    end
+  end
+  
+  private
+  
+   def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+  
 end
