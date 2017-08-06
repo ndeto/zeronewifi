c594373231a09b52922fa95d20121acc90dac580
Martin Ndeto <martindeto@gmail.com>
2017-08-06 18:03:22 +0300

    bootsrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,9 +9,9 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require jquery
 //= require bootstrap-sprockets
-
+//= require bootstrap
+//= require jquery
 //= require jquery_ujs
 //= require bootstrap-colorpicker
 //= require turbolinks
